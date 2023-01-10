7dec53a38999ff9a4c312e18452c98e33b78df11
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-01-09 21:13:54 -0600

    feat: add reverseVowels

---
+++ reverse_vowels/reverse_vowels.js
@@ -0,0 +1,29 @@
+const isVowel = (a) => ["a", "e", "i", "o", "u"].includes(a);
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+  const { normalized, vowels } = s.split("").reduce(
+    (acc, curr) => {
+      if (isVowel(curr)) {
+        acc.normalized.push("");
+        acc.vowels.unshift(curr);
+      } else {
+        acc.normalized.push(curr);
+      }
+      return acc;
+    },
+    {
+      normalized: [],
+      vowels: [],
+    }
+  );
+  return normalized
+    .map((char) => {
+      if (char === "") return vowels.shift();
+      return char;
+    })
+    .join("");
+};
